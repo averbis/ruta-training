bbd9b1694937523ee29d491b6875b9f57a3abe2e
Peter Klügl <peter.kluegl@averbis.com>
2020-01-20 17:45:08 +0100

    fixed test, updated version, added license headers in java files

--- tutorial-projects-solved/unit3-exercise4/src/test/java/uima/ruta/training/FirstTest.java
+++ tutorial-projects-solved/unit3-exercise4/src/test/java/uima/ruta/training/FirstTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package uima.ruta.training;
 
 import java.io.IOException;
@@ -11,22 +29,22 @@ import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.CasUtil;
 import org.apache.uima.jcas.JCas;
+import org.junit.Assert;
 import org.junit.Test;
 
-import junit.framework.Assert;
-
 public class FirstTest {
-	
+
 	@Test
 	public void test() throws IOException, UIMAException {
+
 		JCas jcas = JCasFactory.createJCas();
 		jcas.setDocumentText("some text mentioning Stephen.");
 		AnalysisEngine ae = AnalysisEngineFactory.createEngine("uima.ruta.training.FirstRutaAE");
 		ae.process(jcas);
-		Type type =  jcas.getTypeSystem().getType("uima.ruta.training.Second.MyType");
+		Type type = jcas.getTypeSystem().getType("uima.ruta.training.Second.MyType");
 		Collection<AnnotationFS> select = CasUtil.select(jcas.getCas(), type);
 		Assert.assertEquals(1, select.size());
 		Assert.assertEquals("Stephen", select.iterator().next().getCoveredText());
 	}
-	
+
 }
